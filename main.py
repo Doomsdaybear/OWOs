835d184f5969341115e7220f5e34a43fdbe96d94
Doomsdaybear <101878029+Doomsdaybear@users.noreply.github.com>
2023-07-24 15:32:26 -0400

    OWOs Mega Update 1
    added new functions. All except ./convooo are in help. ./convooo is buggy af so it's not listed as of rn.

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import os
 import time as t
 import sys
+
+def chat() :
+    #very buggy, don't touch yet
+    clear()
+    print("A-a conversation??? Okie then, what should we talk about? \n")
+    keywrd = input("")
+    if "anime" or "Anime" in keywrd :
+        print("Anime. I swear I've heard that word somewhere before.")
+        t.sleep(5)
+        chat()
+    if "love" or "Love" in keywrd :
+        print("w-what???O-O")
+        t.sleep(5)
+        chat()
+    else :
+        print("Sorry, I don't know about that.")
+        t.sleep(5)
+        chat()
+
 def clear() :
+    #simple. Clears terminal
     os.system('cls')
 def Main() :
     clear()
@@ -9,7 +29,7 @@ def Main() :
     cmnd = input("")
     if (cmnd == "./halp") :
         clear()
-        print("Ow Kay!!!!! Hewe's a wist of commandz: \n [1] ./halp (halps you wit using!!!) \n [2] ./integor (makes numbers go up super fast!!! O_O!!!) \n [3] ./lookz (a pic of me!!)")
+        print("Ow Kay!!!!! Hewe's a wist of commandz: \n [1] ./halp (halps you wit using!!!) \n [2] ./integor (makes numbers go up super fast!!! O_O!!!) \n [3] ./lookz (a pic of me!!) \n [4] ./naem (tels me your name!) [5] ./clear (deletes my memory of whatever you told me.)")
         print("Abowrting in 10 secondz!!!!")
         t.sleep(10)
         clear()
@@ -86,5 +106,43 @@ def Main() :
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠚⠀⠀⠈⠓⠤⣀⡀⠀⠀⠀⠀⠀⢀⣠⠔⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠈⠉⠉⠉⠉⠉⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀""")
         t.sleep(5)
+        Main()
+    if cmnd == "./naem" :
+        thefile = open("userinfo.txt", "a")
+        print("Okie! Please write ur naem! \n")
+        global name 
+        name = input("")
+        thefile.write(name)
+        clear()
+        #doesn't work???????????????????????????????????????????????????????????????
+        if name == str(os.getlogin()) :
+            print("There's no need to lie about your name though. Windows here says it's actually " + str(os.getlogin()))
+            t.sleep(3)
+            thefile.close()
+            clear()
+            Main()
+        elif name != str(os.getlogin()) :
+            print("thx!")
+            t.sleep(3)
+            thefile.close()
+            clear()
+            Main()
+
+    if cmnd == "./convooo" :
+        chat()
+    if cmnd == "./clear" :
+        clear()
+        print("Are you sure? This kills my memory >_< (y/n)")
+        yea = input("")
+        if yea == "y" :
+            print("fine... give me a second.")
+            t.sleep(2)
+            #deletes data file from previous session (until i can find a way to prevent overwriting.)
+            os.remove("userinfo.txt")
+            print("There, all done!")
+            t.sleep(2)
+            Main()
+    else :
+        clear()
         Main()
 Main()
